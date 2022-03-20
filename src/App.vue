23a631b2803df5c735412e669557c8fe7daddefe
NatalieCK <270053597@yoobeestudent.ac.nz>
2022-03-20 18:06:51 +1300

    core_content_add

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import Original from './components/OriginalComponent.vue'
+
+
+</script>
+
+<template>
+
+<Original />
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/OriginalComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 class="header">Welcome to group B whoop :D </h1>
+</template>
+
+<style scoped>
+.header {
+    color: hotpink;
+}
+</style>
